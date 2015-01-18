4b1fb83a3e0f5aa7c8170de3b8207629ef397873
Compilenix <Compilenix@compilenix.org>
2015-01-18 21:15:19 +0100

    first testing about html/1.0 "content-Length"

--- requestHandlers/Post.js
+++ requestHandlers/Post.js
@@ -10,23 +10,34 @@ function Post(request, response) {
 
 function post(error, data, request, response) {
 
+    var htmlCode = 404;
+    var dataToSend = "";
     var posts = _fs.readdirSync(DirectoryPosts).reverse();
     var query = _querystring.parse(_url.parse(request.url).query)["p"];
     var letters = new RegExp('^[A-Za-z0-9]+$');
 
     if(query !== undefined && query !== null && query.match(letters)) {
         if (_fs.existsSync(DirectoryPosts + '/' + query + ".html.asc")) {
-            _writeHead["_200"](response);
-            response.write(data);
-            response.write(_fs.readFileSync(DirectoryPosts + '/' + query + '.html', 'utf8'));
+            htmlCode = 200;
+            // _writeHead["_200"](response);
+            dataToSend += data;
+            // response.write(data);
+            dataToSend += _fs.readFileSync(DirectoryPosts + '/' + query + '.html', 'utf8');
+            // response.write(_fs.readFileSync(DirectoryPosts + '/' + query + '.html', 'utf8'));
         } else {
-            _writeHead["_404"](response);
+            htmlCode = 404;
+            dataToSend = '<html><head><title>404 Not Found</title></head><body bgcolor="white"><center><h1>404 Not Found</h1></center><hr><center>node.js/' + process.version + '</center></body></html>';
+            // _writeHead["_404"](response);
         }
     } else {
-        _writeHead["_404"](response);
+        htmlCode = 404;
+        dataToSend = '<html><head><title>404 Not Found</title></head><body bgcolor="white"><center><h1>404 Not Found</h1></center><hr><center>node.js/' + process.version + '</center></body></html>';
+        // _writeHead["_404"](response);
     }
 
-    response.end();
+    // response.writeHead(htmlCode, { "Content-Type": "text/html", "Server": "node.js/" + process.version });
+    response.writeHead(htmlCode, { "Content-Type": "text/html", "Content-Length": dataToSend.length, "Server": "node.js/" + process.version});
+    response.end(dataToSend);
 }
 
 

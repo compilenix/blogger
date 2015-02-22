b63e5692bbb0afe2068e5d19d4656f01f1f9ed5a
Compilenix <Compilenix@compilenix.org>
2015-02-23 00:30:16 +0100

    update index.js; fist generate content than send it to the client

--- requestHandlers/Index.js
+++ requestHandlers/Index.js
@@ -1,3 +1,4 @@
+
 var FileHeader = _Config.post.FileHeader || "header.html";
 var DirectoryPosts = _Config.post.DirectoryPosts || "posts";
 var CountPosts = _Config.post.CountPosts || 10;
@@ -6,15 +7,15 @@ var MessageLastPage = _Config.post.MessageLastPage || "Prev";
 var MessageEnd = _Config.post.MessageEnd || "The end.";
 
 function Index(request, response) {
-    _writeHead["_200"](response);
-
     _fs.readFile(FileHeader, 'utf8', function (error, data) {
         index(error, data, request, response);
     });
 }
 
 function index(error, header, request, response) {
-    response.write(header);
+
+    var htmlCode = 200;
+    var dataToSend = header;
 
     var posts = _helper.getPosts();
     var counter = 0;
@@ -26,7 +27,7 @@ function index(error, header, request, response) {
         queryOffset, offset = 0;
     }
 
-    response.write('\n<ul>\n');
+    dataToSend += '<ul>\n';
 
     for (var i = 0; i < posts.length; i++) {
 
@@ -34,10 +35,10 @@ function index(error, header, request, response) {
                 if (queryOffset > 0) {
                     queryOffset--;
                 } else {
-                    response.write('<li>');
-                    response.write('[<a href="post/?p=' + posts[i] + '">post</a>] ');
-                    response.write(_helper.getPost(posts[i]));
-                    response.write('</li>\n');
+                    dataToSend += '<li>';
+                    dataToSend += '[<a href="post/?p=' + posts[i] + '">post</a>] ';
+                    dataToSend += _helper.getPost(posts[i]);
+                    dataToSend += '</li>\n';
                     counter++;
                     foundSomeThing = true;
                 }
@@ -47,10 +48,10 @@ function index(error, header, request, response) {
         }
     }
 
-    response.write('</ul>\n\n');
+    dataToSend += '</ul>\n\n';
 
     if (foundSomeThing === false || counter < CountPosts) {
-        response.write('<div style="text-align:center"><h2>' + MessageEnd + '</h2></div>');
+        dataToSend += '<div style="text-align:center"><h2>' + MessageEnd + '</h2></div>';
         counter = 1;
         printEndMessage = true;
     }
@@ -58,7 +59,7 @@ function index(error, header, request, response) {
     // print link to last and/or/not to the next and last page.
     var lastPage = (((offset - counter) < 0) ? 0 : (offset - counter));
 
-    response.write('<div style="text-align:center">');
+    dataToSend += '<div style="text-align:center">';
 
     if (lastPage > 0) {
         if (foundSomeThing === false) {
@@ -70,24 +71,28 @@ function index(error, header, request, response) {
         }
 
         if (lastPage < 1) {
-            response.write('<a href=".">' + MessageLastPage + '</a>');
+            dataToSend += '<a href=".">' + MessageLastPage + '</a>';
         } else {
-            response.write('<a href="?offset=' + lastPage + '">' + MessageLastPage + '</a>');
+            dataToSend += '<a href="?offset=' + lastPage + '">' + MessageLastPage + '</a>';
         }
     } else if (foundSomeThing === true && counter === CountPosts) {
-        response.write('<a href=".">' + MessageLastPage + '</a>');
+        dataToSend += '<a href=".">' + MessageLastPage + '</a>';
     }
 
     if ((lastPage > 0 && foundSomeThing === true ) || (foundSomeThing === true && counter === CountPosts)) {
-        response.write(' | ');
+        dataToSend += ' | ';
     }
 
     if (foundSomeThing === true && counter === CountPosts) {
-        response.write('<a href="?offset=' + (offset + counter) + '">' + MessageNextPage + '</a>');
+        dataToSend += '<a href="?offset=' + (offset + counter) + '">' + MessageNextPage + '</a>';
     }
-    response.write('</div>\n</body>\n');
 
-    response.end();
+    // -----------------------------
+
+    dataToSend += "</div>\n</body>\n";
+    
+    response.writeHead(htmlCode, { "Content-Type": "text/html", "Content-Length": dataToSend.length, "Server": "node.js/" + process.version});
+    response.end(dataToSend);
 }
 
 

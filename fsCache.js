18b98c02f9b9d2a17152e65a9704937304177825
Compilenix <Compilenix@compilenix.org>
2015-03-10 18:07:15 +0100

    Move defaults to Config.js

--- fsCache.js
+++ fsCache.js
@@ -1,31 +1,30 @@
-var DirectoryCache = _Config.post.DirectoryCache  || "cache";
+var DirectoryCache = _Config.DirectoryCache  || "cache";
 
 function has(req) {
-    return _fs.existsSync(_path(req));
+	return _fs.existsSync(_path(req));
 }
 
 function send(req, res) {
-    if (has(req)) {
-        var dataToSend = _fs.readFileSync(_path(req), 'utf8');
-        res.setContent(dataToSend);
-        return res;
-    }
+	if (has(req)) {
+		var dataToSend = _fs.readFileSync(_path(req), 'utf8');
+		res.setContent(dataToSend);
+		return res;
+	}
 }
 
 function add(req, cont) {
-    _fs.writeFileSync(_path(req), cont);
+	_fs.writeFileSync(_path(req), cont);
 }
 
 function _hash(req) {
-    var shasum = _crypto.createHash('sha1');
-    shasum.update(req.url);
-    return shasum.digest('hex');
+	var shasum = _crypto.createHash('sha1');
+	shasum.update(req.url);
+	return shasum.digest('hex');
 }
 
 function _path(req) {
-    return DirectoryCache + '/' + _hash(req);
+	return DirectoryCache + '/' + _hash(req);
 }
-
 
 exports.send = send;
 exports.add = add;

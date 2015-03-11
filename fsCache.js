602a5bf6d42ea87fb9402a7c35fb3c9424194680
Compilenix <Compilenix@compilenix.org>
2015-03-11 21:49:18 +0100

    change everything from spaces to tabs.

--- fsCache.js
+++ fsCache.js
@@ -7,8 +7,8 @@ function has(req) {
 function send(req, res) {
 	if (has(req)) {
 		var data = JSON.parse(_fs.readFileSync(_path(req), 'utf8'));
-                        res.setResponseCode(data.response_code);
-                        res.setContentType(data.mime_type);
+		res.setResponseCode(data.response_code);
+		res.setContentType(data.mime_type);
 		res.setContent(data.content);
 		return res;
 	}
@@ -26,11 +26,11 @@ function clear() {
 
 function add(req, cont, mime, code) {
 	_init();
-            var data = JSON.stringify({
-                content: cont,
-                mime_type: mime,
-                response_code:code
-            });
+	var data = JSON.stringify({
+		content: cont,
+		mime_type: mime,
+		response_code:code
+	});
 	_fs.writeFileSync(_path(req), data);
 }
 

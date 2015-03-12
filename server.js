5223f156d7b914f220c96e83b6d10a3b210eacf4
maik alms <maik.email@web.de>
2015-03-12 21:23:08 +0100

    send 404 for unknown callbacks

--- server.js
+++ server.js
@@ -19,12 +19,19 @@ function Start(handle, route) {
 
 function process_request(request, response, handle, route) {
 
+	var pathname = _url.parse(request.url).pathname;
+
+	if (!handle[pathname]) {
+		response.setResponseCode(404);
+		response.send();
+		return false;
+	}
+
 	if (_fscache.has(request) && request.headers["if-modified-since"] === _fscache.getLastModified(request)) {
 		response.setResponseCode(304);
 		response.setLastModified(_fscache.getLastModified(request));
 		response.send();
 	} else {
-		var pathname = _url.parse(request.url).pathname;
 		var callback = handle[pathname].callback;
 
 		var deliver_cache = !(HandleCacheControl && request.headers["cache-control"] === 'no-cache');

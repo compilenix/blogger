eb1a0b78088f3e71270d4287463e8f5e11edfe78
Compilenix <Compilenix@compilenix.org>
2015-03-14 18:42:58 +0100

    remove usage of rfc822 from RSS handler, too

--- requestHandlers/RSS.js
+++ requestHandlers/RSS.js
@@ -42,7 +42,7 @@ function RSS(request, response, write_cache) {
 				// guid
 				dataToSend += "<guid>" + Link + root + "post/?p=" + posts[i] + "</guid>\n";
 				// date
-				dataToSend += "<pubDate>" + _rfc822Date(new Date(parseInt(posts[i], 16) * 1000)) + "</pubDate>\n";
+				dataToSend += "<pubDate>" + new Date(parseInt(posts[i], 16) * 1000).toUTCString() + "</pubDate>\n";
 				// content (html)
 				dataToSend += "<description><![CDATA[" + data + "]]></description>\n"
 

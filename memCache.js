65aa67f03cd6e60be756ee99bd5eacb1527f70ec
Maik Alms <johndoe@compilenix.org>
2015-10-09 22:13:25 +0200

    make memCache a child of nullCache and use es6 class syntax

--- memCache.js
+++ memCache.js
@@ -1,78 +1,80 @@
+var nullCache = require('./nullCache.js').nullCache;
 
-function memCache() {
-	this.cache = {};
-}
-
-memCache.prototype.has = function(req) {
-	return typeof this.cache[this._hash(req)] != 'undefined';
-}
-
-memCache.prototype.send = function(req, res) {
-	if (this.has(req)) {
-		var data = this.cache[this._hash(req)].content;
-		res.setResponseCode(this.cache[this._hash(req)].response_code);
-		res.setContentType(this.cache[this._hash(req)].mime_type);
-		res.setLastModified(this.getLastModified(req));
-		res.setContent(data);
-		res.send();
+class memCache extends nullCache {
+	constructor() {
+		super();
+		this.cache = {};
 	}
-}
 
-memCache.prototype.getLastModified = function(req) {
-	if (this.has(req)) {
-		return (this.cache[this._hash(req)].time).toUTCString();
-	} else {
-		return false;
+	has(req) {
+		return typeof this.cache[this._hash(req)] != 'undefined';
 	}
-}
 
-memCache.prototype.validate = function (req) {
-	var cached = this.cache[this._hash(req)] || false;
-	if (this.has(req) && (cachedDate = (cached.time).getTime())) {
+	send(req, res) {
+		if (this.has(req)) {
+			var data = this.cache[this._hash(req)].content;
+			res.setResponseCode(this.cache[this._hash(req)].response_code);
+			res.setContentType(this.cache[this._hash(req)].mime_type);
+			res.setLastModified(this.getLastModified(req));
+			res.setContent(data);
+			res.send();
+		}
+	}
 
-		for (var i = cached.dependencies.length - 1; i >= 0; i--) {
-			try {
-				if ((_fs.statSync(cached.dependencies[i]).mtime).getTime() > cachedDate) {
+	getLastModified(req) {
+		if (this.has(req)) {
+			return (this.cache[this._hash(req)].time).toUTCString();
+		} else {
+			return false;
+		}
+	}
+
+	validate(req) {
+		var cached = this.cache[this._hash(req)] || false;
+		if (this.has(req) && (cachedDate = (cached.time).getTime())) {z
+			for (var i = cached.dependencies.length - 1; i >= 0; i--) {
+				try {
+					if ((_fs.statSync(cached.dependencies[i]).mtime).getTime() > cachedDate) {
+						return false;
+					}
+				} catch (e) {
 					return false;
 				}
-			} catch (e) {
-				return false;
-			}
-		};
-		return true;
-	} else {
-		return false;
+			};
+			return true;
+		} else {
+			return false;
+		}
 	}
-}
 
-memCache.prototype.del = function (req) {
-	if (this.has(req)) {
-		console.log("Remove cache file: " + this._hash(req));
-		this.cache[this._hash(req)] = undefined;
+	del(req) {
+		if (this.has(req)) {
+			console.log("Remove cache file: " + this._hash(req));
+			this.cache[this._hash(req)] = undefined;
+		}
 	}
-}
 
-memCache.prototype.clear = function() {
-	this.cache = {};
-}
-
-memCache.prototype.add = function(req, cont, mime, code, dependsOn) {
-
-	var data = {
-		mime_type: mime,
-		response_code: code,
-		time: new Date(),
-		dependencies: dependsOn,
-		content: cont
+	clear() {
+		this.cache = {};
 	}
-	console.log("Add mem cache " + this._hash(req));
-	this.cache[this._hash(req)] = data;
+
+	add(req, cont, mime, code, dependsOn) {
+		var data = {
+			mime_type: mime,
+			response_code: code,
+			time: new Date(),
+			dependencies: dependsOn,
+			content: cont
+		}
+		console.log("Add mem cache " + this._hash(req));
+		this.cache[this._hash(req)] = data;
+	}
+
+	_hash(req) {
+		return super._hash(req);
+	}
+
 }
 
-memCache.prototype._hash = function(req) {
-	var shasum = _crypto.createHash('sha1');
-	shasum.update(req.url);
-	return shasum.digest('hex');
-}
 
 exports.memCache = memCache;

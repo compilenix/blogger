7ed4c4580692b32453d788846d5dcab669bc3c4b
Maik Alms <johndoe@compilenix.org>
2015-10-11 00:37:27 +0200

    ws

--- requestHandlers/Edit.js
+++ requestHandlers/Edit.js
@@ -2,7 +2,7 @@ function Edit(request) {
 	return {
 		type: 'content',
 		code: 200,
-		content: _fs.readFileSync(_Config.FileEditor|| "editor.html", 'utf8'),
+		content: _fs.readFileSync(_Config.FileEditor || "editor.html", 'utf8'),
 		mimetype: 'text/html'
 	}
 }

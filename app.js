a56e5f5d2e48a784763961cacdc63a49f6a40b6a
Compilenix <Compilenix@compilenix.org>
2016-08-22 18:11:10 +0200

    Much stuff, again...
    close #34 Editor: Tablist overflow
    close #33 Editor: Wrong title of post when saving
    close #32 Editor: Bug when pasing into the editor
    close #31 Editor: postlist scrollbar overflow missing
    close #30 Editor: missing image at inactive editor tabs
    close #29 static content lacks some http headers
    close #28 application sends every http response twice
    close #26 Adding support for HTTPS (TLS)
    close #25 Adding support for HTTP/2 and "Server Push"

--- app.js
+++ app.js
@@ -1,38 +1,30 @@
-"use strict";
-
-console.log("platform: " + process.platform);
-console.log("architecture: " + process.arch);
-console.log("versions: " + JSON.stringify(process.versions));
-console.log("command line arguments: " + process.argv);
-
-var cluster = require("cluster");
+global.cluster = require("cluster");
 var fs = require("fs");
 
 var ConfigFile = undefined;
 const ConfigDefaultFile = "./Config.js.example";
 if (fs.existsSync("./Config.js")) {
-    ConfigFile = "./Config.js";
+	ConfigFile = "./Config.js";
 } else {
-    ConfigFile = ConfigDefaultFile;
+	ConfigFile = ConfigDefaultFile;
 }
 
-console.log("loading config: " + ConfigFile);
 global.Config = require(ConfigFile).Config;
 global.ConfigDefault = require(ConfigDefaultFile).Config;
 
 process.argv.forEach(function (val, index, array) {
-    switch (val) {
-        case "config":
-            console.log("Loading config from command line!");
-            console.log(process.argv[index + 1]);
-            Config = JSON.parse(process.argv[index + 1]);
-            break;
-    }
+	switch (val) {
+		case "config":
+			console.log("Loading config from command line!");
+			console.log(process.argv[index + 1]);
+			Config = JSON.parse(process.argv[index + 1]);
+			break;
+	}
 });
 
 if (Config.DevMode) {
-    Config.HeaderExpires = 0;
-    Config.ClearCacheOnStart = true;
+	Config.HeaderExpires = 0;
+	Config.ClearCacheOnStart = true;
 }
 
 if (!fs.existsSync(Config.post.DirectoryPosts)) {
@@ -55,16 +47,16 @@ global.requestHandlers = require("./lib/requestHandlers.js");
 
 global.handle = [];
 function Init() {
-    handle.push({ match: /(^\/static\/?.+$)|(\/favicon.ico)/, callback: requestHandlers.Static, cache: false });
-    handle.push({ match: /^\/post\/?.+$/, callback: requestHandlers.Post, cache: true });
-    handle.push({ match: /^\/ajax\/?.+$/, callback: requestHandlers.Ajax, cache: false });
-    handle.push({ match: /^\/rss$|^\/rss.xml$/, callback: requestHandlers.RSS, cache: true });
-    handle.push({ match: /^\/edit$|^\/edit\/$/, callback: requestHandlers.Edit, cache: false });
-    handle.push({ match: /^\/code\/?.+$/, callback: requestHandlers.Code, cache: false });
-    handle.push({ match: /^\/page\/?.+$/, callback: requestHandlers.Page, cache: true });
-    handle.push({ match: /^\/$/, callback: requestHandlers.Index, cache: true });
+	handle.push({ match: /(^\/static\/?.+$)|(\/favicon.ico)/, callback: requestHandlers.Static, cache: false });
+	handle.push({ match: /^\/post\/?.+$/, callback: requestHandlers.Post, cache: true });
+	handle.push({ match: /^\/ajax\/?.+$/, callback: requestHandlers.Ajax, cache: false });
+	handle.push({ match: /^\/rss$|^\/rss.xml$/, callback: requestHandlers.RSS, cache: true });
+	handle.push({ match: /^\/edit$|^\/edit\/$/, callback: requestHandlers.Edit, cache: false });
+	handle.push({ match: /^\/code\/?.+$/, callback: requestHandlers.Code, cache: false });
+	handle.push({ match: /^\/page\/?.+$/, callback: requestHandlers.Page, cache: true });
+	handle.push({ match: /^\/$/, callback: requestHandlers.Index, cache: true });
 
-    server.Start(router.Route);
+	server.Start(router.Route);
 }
 
 require("./lib/cache/NullCache.js");
@@ -72,61 +64,71 @@ require("./lib/cache/FsCache.js");
 require("./lib/cache/MemCache.js");
 
 switch (Config.cache) {
-    case "FsCache":
-        console.log("using FsCache module for caching");
-        global.Cache = new FsCache();
-        break;
-    case "MemCache":
-        console.log("using MemCache module for caching");
-        global.Cache = new MemCache();
-        break;
-    case "none":
-    default:
-        console.log("using no cache");
-        global.Cache = new NullCache();
-        break;
+	case "FsCache":
+		if (cluster.isMaster) {
+			console.log("using FsCache module for caching");
+		}
+		global.Cache = new FsCache();
+		break;
+	case "MemCache":
+		if (cluster.isMaster) {
+			console.log("using MemCache module for caching");
+		}
+		global.Cache = new MemCache();
+		break;
+	case "none":
+	default:
+		if (cluster.isMaster) {
+			console.log("using no cache");
+		}
+		global.Cache = new NullCache();
+		break;
 }
 
 if (Config.Version && Config.Version !== ConfigDefault.Version) {
-    console.log("Update detected -> clear cache...");
-    Cache.clear();
-    console.log("Please update your Config.js!");
+	console.log("Config version difference detected (reference was \"" + ConfigDefaultFile + "\") -> clearing cache...");
+	Cache.clear();
+	console.log("Please update your Config.js!");
 	process.exit(1);
 }
 
 if (Config.DevMode) {
-    if (Cache && Config.ClearCacheOnStart) {
-        Cache.clear();
-    }
-    Init();
+	if (Cache && Config.ClearCacheOnStart) {
+		Cache.clear();
+	}
+	Init();
 } else {
-    if (cluster.isMaster) {
+	if (cluster.isMaster) {
+		console.log("platform: " + process.platform);
+		console.log("architecture: " + process.arch);
+		console.log("versions: " + JSON.stringify(process.versions));
+		console.log("command line arguments: " + process.argv);
 
-        if (Cache && Config.ClearCacheOnStart) {
-            Cache.clear();
-        }
+		if (Cache && Config.ClearCacheOnStart) {
+			Cache.clear();
+		}
 
-        if (Config.threads <= 1) {
-            Config.threads = 1;
-        }
+		if (Config.threads <= 1) {
+			Config.threads = 1;
+		}
 
-        for (var i = Config.threads; i > 0; i--) {
-            cluster.fork();
-        }
+		for (var i = Config.threads; i > 0; i--) {
+			cluster.fork();
+		}
 
-        cluster.on("fork", function (worker) {
-            console.log("worker #%d forked. (pid %d)", worker.id, worker.process.pid);
-        });
+		cluster.on("fork", function (worker) {
+			console.log("worker #%d forked. (pid %d)", worker.id, worker.process.pid);
+		});
 
-        cluster.on("disconnect", function (worker) {
-            console.log("worker #%d (pid %d) disconnected.", worker.id, worker.process.pid);
-        });
+		cluster.on("disconnect", function (worker) {
+			console.log("worker #%d (pid %d) disconnected.", worker.id, worker.process.pid);
+		});
 
-        cluster.on("exit", function (worker, code, signal) {
-            console.log("worker #%d (pid %d) died (returned code %s; signal %s). restarting...", worker.id, worker.process.pid, code || "undefined", signal || "undefined");
-            cluster.fork();
-        });
-    } else if (cluster.isWorker) {
-        Init();
-    }
+		cluster.on("exit", function (worker, code, signal) {
+			console.log("worker #%d (pid %d) died (returned code %s; signal %s). restarting...", worker.id, worker.process.pid, code || "undefined", signal || "undefined");
+			cluster.fork();
+		});
+	} else if (cluster.isWorker) {
+		Init();
+	}
 }
--- server.js
+++ server.js
@@ -1,67 +1,92 @@
-﻿var Port = Config.server.port || 80;
+var Port = Config.server.port || 80;
 var HandleClientCacheControl = Config.HandleClientCacheControl || true;
 var DevMode = Config.DevMode || false;
 
-var http = require("http");
+var http = undefined;
+var https = undefined;
+var httpsOptions = {
+	key: Config.Https.Key,
+	cert: Config.Https.Cert
+};
+
+if (Config.EnableHttp2 && Config.Https.Enabled) {
+	console.log("Start server with https AND h2 enabled");
+	https = require("http2");
+} else if (Config.Https.Enabled) {
+	console.log("Start server with https enabled");
+	https = require("https");
+} else {
+	console.log("Start server with http");
+	http = require("http");
+}
+
 var domain = require("domain");
 var url = require("url");
 var fs = require("fs");
 var querystring = require("querystring");
 
 function Start(route) {
-    var onErrRes = undefined;
+	var onErrRes = undefined;
 
-    function onRequest(request, response) {
-        const res = new ResponseWrapper(response);
+	function onRequest(request, response) {
+		const res = new ResponseWrapper(response);
 
-        if (!DevMode) {
-            onErrRes = res;
-        }
+		if (!DevMode) {
+			onErrRes = res;
+		}
 
-        process_request(request, res, route);
-    }
+		process_request(request, res, route);
+	}
 
-    function onErr(error) {
-        console.error("error", error.stack);
-        onErrRes.setResponseCode(500);
-        onErrRes.setContent("");
-        ResponseCodeMessage.ResponseCodeMessage(onErrRes);
-        onErrRes.send();
-        process.exit(1);
-    }
+	function onErr(error) {
+		console.error("error", error.stack);
+		onErrRes.setResponseCode(500);
+		onErrRes.setContent("");
+		ResponseCodeMessage.ResponseCodeMessage(onErrRes);
+		onErrRes.send();
+		process.exit(1);
+	}
 
-	console.log("Starting Server on port: " + Port);
-    if (DevMode) {
-        http.createServer(onRequest).listen(Port);
-    } else {
-        const currentDomain = domain.create();
-        currentDomain.on("error", onErr);
-        currentDomain.run(function() {
-            http.createServer(onRequest).listen(Port);
-        });
-    }
-    console.log("Init done, waiting for clients/requests...");
+	if (DevMode) {
+		console.log("Starting Server on port: " + Port);
+		if ((Config.EnableHttp2 && Config.Https.Enabled) || Config.Https.Enabled) {
+			https.createServer(httpsOptions, onRequest).listen(Port);
+		} else {
+			http.createServer(onRequest).listen(Port);
+		}
+	} else {
+		const currentDomain = domain.create();
+		currentDomain.on("error", onErr);
+
+		currentDomain.run(function() {
+			console.log("Starting Server on port: " + Port);
+			if ((Config.EnableHttp2 && Config.Https.Enabled) || Config.Https.Enabled) {
+				https.createServer(httpsOptions, onRequest).listen(Port);
+			} else {
+				http.createServer(onRequest).listen(Port);
+			}
+		});
+	}
+
+	console.log("Init done, waiting for clients/requests...");
 }
 
-/*
- * returns bool which is not relevant for the calling instance, used to break the execution
- */
 function process_request(request, response, route) {
-    const pathname = url.parse(request.url).pathname;
+	const pathname = url.parse(request.url).pathname;
 
 	if (Config.DevMode) {
 		console.log("process_request: " + request.url);
 	}
 
-    if (!router.RouteExists(pathname)) {
-        if (Config.DevMode) {
-            console.log("404: " + (pathname == undefined ? "undefined" : pathname));
-        }
-        response.setResponseCode(404);
-        ResponseCodeMessage.ResponseCodeMessage(response);
-        response.send();
-        return false;
-    }
+	if (!router.RouteExists(pathname)) {
+		if (Config.DevMode) {
+			console.log("404: " + (pathname == undefined ? "undefined" : pathname));
+		}
+		response.setResponseCode(404);
+		ResponseCodeMessage.ResponseCodeMessage(response);
+		response.send();
+		return false;
+	}
 
 	if (router.GetRoute(pathname).callback === requestHandlers.Static && (request.headers["cache-control"] !== "no-cache")) {
 		const path = Helper.replaceAll("/", Helper.GetFsDelimiter(), Config.staticContentPath);
@@ -82,58 +107,56 @@ function process_request(request, response, route) {
 
 	let cacheLastModified = Cache.getLastModified(request);
 	let cacheHasRequest = Cache.has(request);
-    if (cacheHasRequest && request.headers["if-modified-since"] === cacheLastModified) {
-        response.setResponseCode(304);
-        response.setLastModified(cacheLastModified);
-        response.send();
-    } else {
-        const deliverCache = !(HandleClientCacheControl && request.headers["cache-control"] === "no-cache");
-        const writeCache = router.RouteGetCacheEnabled(pathname);
-        if (deliverCache && cacheHasRequest) {
-            Cache.send(request, response);
-            return false;
-        }
+	if (cacheHasRequest && request.headers["if-modified-since"] === cacheLastModified) {
+		response.setResponseCode(304);
+		response.setLastModified(cacheLastModified);
+		response.send();
+	} else {
+		const deliverCache = !(HandleClientCacheControl && request.headers["cache-control"] === "no-cache");
+		const writeCache = router.RouteGetCacheEnabled(pathname);
+		if (deliverCache && cacheHasRequest) {
+			Cache.send(request, response);
+			return false;
+		}
 
-        var data = route(pathname, request);
+		var data = route(pathname, request);
 
-        if (request.method === "POST") {
-            var body = "";
+		if (request.method === "POST") {
+			var body = "";
 
-            request.on("data", function(postData) {
-                // reading http POST body
-                if (body.length + postData.length < Config.MaxHttpPOSTSize) {
-                    body += postData;
-                } else {
-                    // Request entity too large
-                    response.setResponseCode(413);
-                    ResponseCodeMessage.ResponseCodeMessage(response);
-                    response.send();
-                    return false;
-                }
-                return true;
-            });
+			request.on("data", function(postData) {
+				// reading http POST body
+				if (body.length + postData.length < Config.MaxHttpPOSTSize) {
+					body += postData;
+				} else {
+					// Request entity too large
+					response.setResponseCode(413);
+					ResponseCodeMessage.ResponseCodeMessage(response);
+					response.send();
+					return false;
+				}
+				return true;
+			});
 
-            request.on("end", function() {
-                // after reading the http POST body, call the callback function "data()" from request handler
-                sendData(data(body, request), request, response, writeCache);
-            });
-            return true;
-        } else if (request.method === "GET") {
-            if (!sendData(data, request, response, writeCache)) {
-                return false;
-            }
-        } else {
-            // unknown http method
-            response.setResponseCode(501);
-            ResponseCodeMessage.ResponseCodeMessage(response);
-            response.send();
-            return false;
-        }
+			request.on("end", function() {
+				// after reading the http POST body, call the callback function "data()" from request handler
+				sendData(data(body, request), request, response, writeCache);
+			});
+			return true;
+		} else if (request.method === "GET") {
+			if (!sendData(data, request, response, writeCache)) {
+				return false;
+			}
+		} else {
+			// unknown http method
+			response.setResponseCode(501);
+			ResponseCodeMessage.ResponseCodeMessage(response);
+			response.send();
+			return false;
+		}
+	}
 
-        response.send();
-    }
-
-    return true;
+	return true;
 }
 
 /*
@@ -141,42 +164,48 @@ function process_request(request, response, route) {
  * returns false if response has been send
  */
 function sendData(data, request, response, writeCache) {
-    if (data && data.type) {
-        if (data.mimetype) {
-            response.setContentType(data.mimetype);
-        }
+	if (data && data.type) {
+		if (data.mimetype) {
+			response.setContentType(data.mimetype);
+		}
 
-        if (data.code) {
-            response.setResponseCode(data.code);
-        }
+		if (data.code) {
+			response.setResponseCode(data.code);
+		}
 
-        if (data.type === "file" && data.content) {
-            response.setResponseCode(data.code || 200);
+		if (data.type === "file" && data.content) {
+			response.setResponseCode(data.code || 200);
 			response.setLastModified(new Date(fs.statSync(data.content).mtime).toUTCString());
 			response.contentLength = fs.statSync(data.content).size;
-            response.sendFileStream(fs.createReadStream(data.content));
-            return false;
-        }
+			response.sendFileStream(fs.createReadStream(data.content));
+			return false;
+		}
 
-        if (data.content) {
-            response.setContent(data.content);
-        }
+		if (data.content) {
+			response.setContent(data.content);
+		}
 
-        ResponseCodeMessage.ResponseCodeMessage(response);
+		ResponseCodeMessage.ResponseCodeMessage(response);
 
-        if (writeCache) {
-            Cache.add(request, data.content, data.mimetype, data.code);
-            response.setLastModified(Cache.getLastModified(request));
-        }
+		if (writeCache) {
+			Cache.add(request, data.content, data.mimetype, data.code);
+			response.setLastModified(Cache.getLastModified(request));
+		}
+
+		if (Config.EnableHttp2 && Config.Https.Enabled && data.push) {
+			data.push.forEach(function(element) {
+				response.addHttp2PushEntity(element.path, element.data, element.httpCode, element.header);
+			}, this);
+		}
 
 		response.send();
 
-        return true;
-    } else {
-        ResponseCodeMessage.ResponseCodeMessage(response);
-        response.send();
-        return false;
-    }
+		return true;
+	} else {
+		ResponseCodeMessage.ResponseCodeMessage(response);
+		response.send();
+		return false;
+	}
 }
 
 

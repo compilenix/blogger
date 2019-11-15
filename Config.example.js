a05bdff3e7f61730d349fef961852ec19a144ea2
CompileNix <compilenix@gmail.com>
2019-11-15 20:35:23 +0100

    close #46: code format

--- Config.example.js
+++ Config.example.js
@@ -1,116 +1,115 @@
-const os = require("os");
-const fs = require("fs");
+const os = require('os')
+const fs = require('fs')
 
 class Config {
-	constructor() {
-		this.Version = "1";
-		this.root = "/";
-		this.language = "en";
-		this.author = "user";
-		this.authorMail = "user@domain.tld";
-		this.Link = "http://127.0.0.1"; // no trailing slash!
-		this.Title = "My Blog";
-		this.DirectoryCache = "cache";
-		this.HeaderExpires = 60000 * 60; // 60 Minutes
-		this.ClearCacheOnStart = false;
-		this.DevMode = false; // will be set to true if process.env.NODE_ENV === "development"
-		this.MaxHttpPOSTSize = 5e7; // 50 Megabyte
-		this.HandleClientCacheControl = true;
-		this.DefaultContentType = "text/html";
-		this.ServerVersion = `node.js/${process.version}`;
-        this.SendServerVersionInHeader = false;
-        this.Cache = "FsCache"; // "MemCache" | "FsCache" | "none"
-		this.FileEditor = "editor.html";
-		this.ApiKey = "changeme";
+  constructor () {
+    this.Version = '1'
+    this.root = '/'
+    this.language = 'en'
+    this.author = 'user'
+    this.authorMail = 'user@domain.tld'
+    this.Link = 'http://127.0.0.1' // no trailing slash!
+    this.Title = 'My Blog'
+    this.DirectoryCache = 'cache'
+    this.HeaderExpires = 60000 * 60 // 60 Minutes
+    this.ClearCacheOnStart = false
+    this.DevMode = false // will be set to true if process.env.NODE_ENV === "development"
+    this.MaxHttpPOSTSize = 5e7 // 50 Megabyte
+    this.HandleClientCacheControl = true
+    this.DefaultContentType = 'text/html'
+    this.ServerVersion = `node.js/${process.version}`
+    this.SendServerVersionInHeader = false
+    this.Cache = 'FsCache' // "MemCache" | "FsCache" | "none"
+    this.FileEditor = 'editor.html'
+    this.ApiKey = 'changeme'
 
-		this.Log = {
-			Level: "Info", // "Debug", "Info", "Warn", "Error"
-			File: {
-				Enabled: true,
-				Path: "log",
-				FilePerLoglevel: true
-			},
-			Console: {
-				Enabled: true
-			}
-		};
+    this.Log = {
+      Level: 'Info', // "Debug", "Info", "Warn", "Error"
+      File: {
+        Enabled: true,
+        Path: 'log',
+        FilePerLoglevel: true
+      },
+      Console: {
+        Enabled: true
+      }
+    }
 
-		this.Server = {
-			Port: 8000,
-			UseMultipleProcessingCores: true,
-			Threads: os.cpus().length // is ignored if "UseMultipleProcessingCores" is set to "false" or "DevMode" is set to "true"
-		};
+    this.Server = {
+      Port: 8000,
+      UseMultipleProcessingCores: true,
+      Threads: os.cpus().length // is ignored if "UseMultipleProcessingCores" is set to "false" or "DevMode" is set to "true"
+    }
 
-		this.Https = {
-			Enabled: false,
-			Key: fs.readFileSync("./localhost.key"),
-			Cert: fs.readFileSync("./localhost.crt")
-		};
-		this.EnableHttp2 = false; // for this option you must enable and configure "Https" otherwise the application will automatically fallback to plain HTTP!
+    this.Https = {
+      Enabled: false,
+      Key: fs.readFileSync('./localhost.key'),
+      Cert: fs.readFileSync('./localhost.crt')
+    }
+    this.EnableHttp2 = false // for this option you must enable and configure "Https" otherwise the application will automatically fallback to plain HTTP!
 
-		this.post = {
-			FileHeader: "header.html",
-			FileFooter: "footer.html",
-			DirectoryPosts: "posts",
-			CountPosts: 20,
-			MessageOlderPage: "Older",
-			MessageNewerPage: "Newer",
-			essageEnd: "The end."
-		};
+    this.post = {
+      FileHeader: 'header.html',
+      FileFooter: 'footer.html',
+      DirectoryPosts: 'posts',
+      CountPosts: 20,
+      MessageOlderPage: 'Older',
+      MessageNewerPage: 'Newer',
+      essageEnd: 'The end.'
+    }
 
-		this.rss = {
-			CountPosts: 100,
-			Description: "Some Description",
-			Encoding: "UTF-8",
-			skipHours: [0, 1, 2, 3, 4, 5, 6, 7],
-			Title: "Some Title",
-			ttl: 60, // minutes
-			webMaster: "Admin",
-			webMasterMail: "admin@domain.tld"
-		};
+    this.rss = {
+      CountPosts: 100,
+      Description: 'Some Description',
+      Encoding: 'UTF-8',
+      skipHours: [0, 1, 2, 3, 4, 5, 6, 7],
+      Title: 'Some Title',
+      ttl: 60, // minutes
+      webMaster: 'Admin',
+      webMasterMail: 'admin@domain.tld'
+    }
 
-		this.Fonts = {
-			Ubuntu: {
-				Regular: "Ubuntu-R.ttf",
-				Bold: "Ubuntu-B.ttf"
-			},
-			SourceCodePro: {
-				Regular: {
-					EmbeddedOpentype: "SourceCodePro-Regular.eot",
-					Woff2: "SourceCodePro-Regular.ttf.woff2",
-					Woff: "SourceCodePro-Regular.otf.woff",
-					Opentype: "SourceCodePro-Regular.otf",
-					Truetype: "SourceCodePro-Regular.ttf"
-				},
-				Bold: {
-					EmbeddedOpentype: "SourceCodePro-Bold.eot",
-					Woff2: "SourceCodePro-Bold.ttf.woff2",
-					Woff: "SourceCodePro-Bold.otf.woff",
-					Opentype: "SourceCodePro-Bold.otf",
-					Truetype: "SourceCodePro-Bold.ttf"
-				},
-				Italic: {
-					EmbeddedOpentype: "SourceCodePro-It.eot",
-					Woff2: "SourceCodePro-It.ttf.woff2",
-					Woff: "SourceCodePro-It.otf.woff",
-					Opentype: "SourceCodePro-It.otf",
-					Truetype: "SourceCodePro-It.ttf"
-				},
-				BoldItalic: {
-					EmbeddedOpentype: "SourceCodePro-BoldIt.eot",
-					Woff2: "SourceCodePro-BoldIt.ttf.woff2",
-					Woff: "SourceCodePro-BoldIt.otf.woff",
-					Opentype: "SourceCodePro-BoldIt.otf",
-					Truetype: "SourceCodePro-BoldIt.ttf"
-				}
-			}
-		};
+    this.Fonts = {
+      Ubuntu: {
+        Regular: 'Ubuntu-R.ttf',
+        Bold: 'Ubuntu-B.ttf'
+      },
+      SourceCodePro: {
+        Regular: {
+          EmbeddedOpentype: 'SourceCodePro-Regular.eot',
+          Woff2: 'SourceCodePro-Regular.ttf.woff2',
+          Woff: 'SourceCodePro-Regular.otf.woff',
+          Opentype: 'SourceCodePro-Regular.otf',
+          Truetype: 'SourceCodePro-Regular.ttf'
+        },
+        Bold: {
+          EmbeddedOpentype: 'SourceCodePro-Bold.eot',
+          Woff2: 'SourceCodePro-Bold.ttf.woff2',
+          Woff: 'SourceCodePro-Bold.otf.woff',
+          Opentype: 'SourceCodePro-Bold.otf',
+          Truetype: 'SourceCodePro-Bold.ttf'
+        },
+        Italic: {
+          EmbeddedOpentype: 'SourceCodePro-It.eot',
+          Woff2: 'SourceCodePro-It.ttf.woff2',
+          Woff: 'SourceCodePro-It.otf.woff',
+          Opentype: 'SourceCodePro-It.otf',
+          Truetype: 'SourceCodePro-It.ttf'
+        },
+        BoldItalic: {
+          EmbeddedOpentype: 'SourceCodePro-BoldIt.eot',
+          Woff2: 'SourceCodePro-BoldIt.ttf.woff2',
+          Woff: 'SourceCodePro-BoldIt.otf.woff',
+          Opentype: 'SourceCodePro-BoldIt.otf',
+          Truetype: 'SourceCodePro-BoldIt.ttf'
+        }
+      }
+    }
 
-		this.staticContentPath = "static/";
-		this.staticContentUri = "/static/?f=";
-		this.templatePath = "templates/";
-	}
+    this.staticContentPath = 'static/'
+    this.staticContentUri = '/static/?f='
+    this.templatePath = 'templates/'
+  }
 }
 
-
-module.exports = new Config();
+module.exports = new Config()
--- index.js
+++ index.js
@@ -1,189 +1,190 @@
-require("use-strict");
-const util = require("util");
-const cluster = require("cluster");
-const fs = require("fs-extra");
+'use-strict'
+const util = require('util')
+const cluster = require('cluster')
+const fs = require('fs-extra')
 
-if (!fs.existsSync("./Config.js")) {
-    fs.copySync("./Config.example.js", "./Config.js");
+if (!fs.existsSync('./Config.js')) {
+  fs.copySync('./Config.example.js', './Config.js')
 }
 
-const Server = require("./lib/Server.js");
-const RequestHandler = require("./lib/RequestHandler.js");
-const NullCache = require("./lib/Cache/NullCache.js");
-const FsCache = require("./lib/Cache/FsCache.js");
-const MemCache = require("./lib/Cache/MemCache.js");
+const Server = require('./lib/Server.js')
+const RequestHandler = require('./lib/RequestHandler.js')
+const NullCache = require('./lib/Cache/NullCache.js')
+const FsCache = require('./lib/Cache/FsCache.js')
+const MemCache = require('./lib/Cache/MemCache.js')
 
-const log = require("./lib/LogHandler.js");
-let config = require("./Config.js");
+const log = require('./lib/LogHandler.js')
+let config = require('./Config.js')
 
 process.argv.forEach((val, index) => {
-    switch (val) {
-        case "config":
-            log.info("Loading config from command line!");
-            log.info(process.argv[index + 1]);
-            config = JSON.parse(process.argv[index + 1]);
-            break;
-    }
-});
+  switch (val) {
+    case 'config':
+      log.info('Loading config from command line!')
+      log.info(process.argv[index + 1])
+      config = JSON.parse(process.argv[index + 1])
+      break
+  }
+})
 
-if (process.env.NODE_ENV === "development") {
-    config.DevMode = true;
+if (process.env.NODE_ENV === 'development') {
+  config.DevMode = true
 }
 
 if (config.DevMode) {
-    config.HeaderExpires = 0;
-    config.ClearCacheOnStart = true;
+  config.HeaderExpires = 0
+  config.ClearCacheOnStart = true
 }
 
 if (!fs.existsSync(config.post.DirectoryPosts)) {
-    log.info("Posts-Directory is non-existing! creating new empty directory");
+  log.info('Posts-Directory is non-existing! creating new empty directory')
 
-    try {
-        fs.mkdirSync(config.post.DirectoryPosts);
-    } catch (error) {
-        log.error("Posts-Directory couldn't be created, see following error", error);
-        process.exit(1);
-    }
+  try {
+    fs.mkdirSync(config.post.DirectoryPosts)
+  } catch (error) {
+    log.error("Posts-Directory couldn't be created, see following error", error)
+    process.exit(1)
+  }
 }
 
-const server = new Server();
+const server = new Server()
 
 // TODO move cache from each worker to master
-let cache = new NullCache();
+let cache = new NullCache()
 switch (config.Cache) {
-    case "FsCache":
-        if (cluster.isMaster) {
-            log.info("using FsCache module for caching");
-        }
-        cache = new FsCache();
-        break;
-    case "MemCache":
-        if (cluster.isMaster) {
-            log.info("using MemCache module for caching");
-        }
-        cache = new MemCache();
-        break;
-    default:
-        if (cluster.isMaster) {
-            log.info("using no cache");
-        }
-        break;
+  case 'FsCache':
+    if (cluster.isMaster) {
+      log.info('using FsCache module for caching')
+    }
+    cache = new FsCache()
+    break
+  case 'MemCache':
+    if (cluster.isMaster) {
+      log.info('using MemCache module for caching')
+    }
+    cache = new MemCache()
+    break
+  default:
+    if (cluster.isMaster) {
+      log.info('using no cache')
+    }
+    break
 }
 
-const requestHandler = new RequestHandler(server);
-const handle = new Array();
+const requestHandler = new RequestHandler(server)
+/** @type {object[]} */
+const handle = []
 
-function StartServer() {
-    handle.push({
-        match: /(^\/static\/?.+$)|(\/favicon\.ico)|(\/worker-html\.js)/,
-        handler: requestHandler.get("Static"),
-        cache: false
-    });
-    handle.push({
-        match: /^\/post\/?.+$/,
-        handler: requestHandler.get("Post"),
-        cache: true
-    });
-    handle.push({
-        match: /^\/ajax\/?.+$/,
-        handler: requestHandler.get("Ajax"),
-        cache: false
-    });
-    handle.push({
-        match: /^\/rss$|^\/rss.xml$/,
-        handler: requestHandler.get("Rss"),
-        cache: true
-    });
-    handle.push({
-        match: /^\/edit$|^\/edit\/$/,
-        handler: requestHandler.get("Edit"),
-        cache: false
-    });
-    handle.push({
-        match: /^\/code\/?.+$/,
-        handler: requestHandler.get("Code"),
-        cache: false
-    });
-    handle.push({
-        match: /^\/page\/?.+$/,
-        handler: requestHandler.get("Page"),
-        cache: true
-    });
-    handle.push({
-        match: /^(\/find\/?.+)|(\/search\/?.+)|(\/\?q=)|(\/\?search=)$/,
-        handler: requestHandler.get("Find"),
-        cache: true
-    });
-    handle.push({
-        match: /^\/$/,
-        handler: requestHandler.get("Index"),
-        cache: true
-    });
+function StartServer () {
+  handle.push({
+    match: /(^\/static\/?.+$)|(\/favicon\.ico)|(\/worker-html\.js)/,
+    handler: requestHandler.get('Static'),
+    cache: false
+  })
+  handle.push({
+    match: /^\/post\/?.+$/,
+    handler: requestHandler.get('Post'),
+    cache: true
+  })
+  handle.push({
+    match: /^\/ajax\/?.+$/,
+    handler: requestHandler.get('Ajax'),
+    cache: false
+  })
+  handle.push({
+    match: /^\/rss$|^\/rss.xml$/,
+    handler: requestHandler.get('Rss'),
+    cache: true
+  })
+  handle.push({
+    match: /^\/edit$|^\/edit\/$/,
+    handler: requestHandler.get('Edit'),
+    cache: false
+  })
+  handle.push({
+    match: /^\/code\/?.+$/,
+    handler: requestHandler.get('Code'),
+    cache: false
+  })
+  handle.push({
+    match: /^\/page\/?.+$/,
+    handler: requestHandler.get('Page'),
+    cache: true
+  })
+  handle.push({
+    match: /^(\/find\/?.+)|(\/search\/?.+)|(\/\?q=)|(\/\?search=)$/,
+    handler: requestHandler.get('Find'),
+    cache: true
+  })
+  handle.push({
+    match: /^\/$/,
+    handler: requestHandler.get('Index'),
+    cache: true
+  })
 
-    if (!fs.existsSync(config.templatePath)) {
-        log.info("template-Directory is non-existing! creating new empty directory");
+  if (!fs.existsSync(config.templatePath)) {
+    log.info('template-Directory is non-existing! creating new empty directory')
 
-        try {
-            fs.mkdirSync(config.templatePath);
-        } catch (error) {
-            log.error("template-Directory couldn't be created, see following error", error);
-            process.exit(1);
-        }
+    try {
+      fs.mkdirSync(config.templatePath)
+    } catch (error) {
+      log.error("template-Directory couldn't be created, see following error", error)
+      process.exit(1)
     }
+  }
 
-    server.setCacheModule(cache);
-    server.setRequestHandlers(handle);
-    server.start();
+  server.setCacheModule(cache)
+  server.setRequestHandlers(handle)
+  server.start()
 }
 
 // TODO config.Version !== ConfigDefault.Version
 // if (config.Version && config.Version !== ConfigDefault.Version) {
-// 	logger.info("Config version difference detected (reference was \"" + configDefaultFile + "\") -> clearing cache...");
-// 	Cache.clear();
-// 	logger.info("Please update your Config.js!");
-// 	process.exit(1);
+//   logger.info('Config version difference detected (reference was "' + configDefaultFile + '") -> clearing cache...')
+//   Cache.clear()
+//   logger.info('Please update your Config.js!')
+//   process.exit(1)
 // }
 
 if (config.DevMode) {
-    if (cache && config.ClearCacheOnStart) {
-        cache.clear();
-    }
+  if (cache && config.ClearCacheOnStart) {
+    cache.clear()
+  }
 
-    StartServer();
+  StartServer()
 } else if (!config.Server.UseMultipleProcessingCores) {
-    StartServer();
+  StartServer()
 } else {
-    if (cluster.isMaster) {
-        log.info(`platform: ${process.platform}`);
-        log.info(`architecture: ${process.arch}`);
-        log.info(`versions: ${JSON.stringify(process.versions)}`);
-        log.info(`command line arguments: ${process.argv}`);
+  if (cluster.isMaster) {
+    log.info(`platform: ${process.platform}`)
+    log.info(`architecture: ${process.arch}`)
+    log.info(`versions: ${JSON.stringify(process.versions)}`)
+    log.info(`command line arguments: ${process.argv}`)
 
-        if (cache && config.ClearCacheOnStart) {
-            cache.clear();
-        }
-
-        if (config.Server.Threads <= 1) {
-            config.Server.Threads = 1;
-        }
-
-        for (let i = config.Server.Threads; i > 0; i--) {
-            cluster.fork();
-        }
-
-        cluster.on("fork", function (worker) {
-            log.info(util.format("worker #%d forked. (pid %d)", worker.id, worker.process.pid));
-        });
-
-        cluster.on("disconnect", function (worker) {
-            log.info(util.format("worker #%d (pid %d) disconnected.", worker.id, worker.process.pid));
-        });
-
-        cluster.on("exit", function (worker, code, signal) {
-            log.info(util.format("worker #%d (pid %d) died (returned code %s; signal %s). restarting...", worker.id, worker.process.pid, code || "undefined", signal || "undefined"));
-            cluster.fork();
-        });
-    } else if (cluster.isWorker) {
-        StartServer();
+    if (cache && config.ClearCacheOnStart) {
+      cache.clear()
     }
+
+    if (config.Server.Threads <= 1) {
+      config.Server.Threads = 1
+    }
+
+    for (let i = config.Server.Threads; i > 0; i--) {
+      cluster.fork()
+    }
+
+    cluster.on('fork', function (worker) {
+      log.info(util.format('worker #%d forked. (pid %d)', worker.id, worker.process.pid))
+    })
+
+    cluster.on('disconnect', function (worker) {
+      log.info(util.format('worker #%d (pid %d) disconnected.', worker.id, worker.process.pid))
+    })
+
+    cluster.on('exit', function (worker, code, signal) {
+      log.info(util.format('worker #%d (pid %d) died (returned code %s; signal %s). restarting...', worker.id, worker.process.pid, code || 'undefined', signal || 'undefined'))
+      cluster.fork()
+    })
+  } else if (cluster.isWorker) {
+    StartServer()
+  }
 }
